6d4eb1d47a2defe11e5405052c2624d549d0d65e
3065831722@qq.com <3065831722@qq.com>
2022-06-17 16:40:38 +0800

    tianjiakeshe

--- code-2.py
+++ code-2.py
@@ -11,18 +11,18 @@ wb=xlrd2.open_workbook('机器学习数据.xlsx')
 sheet1_content1=wb.sheet_by_index(0)
 
 target=[]#标记列表
-for i in range(1,53):
+for i in range(1,60):
    # print(sheet1_content1.cell(i,0).value)
-   target.append(round(sheet1_content1.cell(i,0).value,2))
+   target.append(sheet1_content1.cell(i,0).value)
 
 print(target)
 
 data=[]
 # print(sheet1_content1.cell(1,6).value)
-for i in range(1,53):
+for i in range(1,60):
     temp=[]
-    for j in range(8,57):
-        temp.append(round(sheet1_content1.cell(i,j).value,2))
+    for j in range(8,57):#8,57
+        temp.append(sheet1_content1.cell(i,j).value)
     data.append(temp)
 print(data)
 
@@ -30,13 +30,12 @@ x=np.array(data)
 y=np.array(target)
 
 
-
-
-train_data,test_data=train_test_split(x,random_state=1,train_size=0.8,test_size=0.2)
-train_label,test_label=train_test_split(y,random_state=1,train_size=0.8,test_size=0.2)
+train_data,test_data=train_test_split(x,random_state=4,train_size=0.8,test_size=0.2)
+train_label,test_label=train_test_split(y,random_state=4,train_size=0.8,test_size=0.2)
 
 classifier=svm.SVC(C=0.65,kernel='rbf',gamma='auto',decision_function_shape='ovo')
-classifier.fit(train_data,train_label.ravel())
+classifier.fit(train_data,train_label)
+
 
 #模型预测
 pre_train=classifier.predict(train_data)
@@ -44,6 +43,4 @@ pre_test=classifier.predict(test_data)
 print("train准确率：",accuracy_score(train_label,pre_train))
 print("test准确率：",accuracy_score(test_label,pre_test))
 
-#混淆矩阵
-cm=confusion_matrix(test_label,pre_test)
-print("混淆矩阵：",cm)
+
